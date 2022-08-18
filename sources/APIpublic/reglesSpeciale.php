2255f0fbf99b7c1fdcbdd2371fecd1c317c2426a
Calmes <77333391+ChristopherKerhman@users.noreply.github.com>
2022-08-18 20:22:38 +0200

    Fin API

---
+++ sources/APIpublic/reglesSpeciale.php
@@ -0,0 +1,14 @@
+<?php
+include 'APIHeader.php';
+$typeRS = filter($_GET['typeRS']);
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+  // Requête
+  $select = "SELECT `idRS`, `nomRS`, `texteRS` FROM `reglesSpeciales` WHERE `typeRS` = :typeRS";
+              $param = [['prep'=>':typeRS', 'variable'=>$typeRS]];
+  $readData = new RCUD($select, $param);
+  $dataUnivers = $readData->READ();
+  $data_Univers_JSON = JSON_encode($dataUnivers);
+  echo $data_Univers_JSON;
+} else {
+  echo json_encode(['error' => 'Aucune donnée']);
+}
