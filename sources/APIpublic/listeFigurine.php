c1fb6feea1eec782d4949d56163534a7af8968b4
Calmes <77333391+ChristopherKerhman@users.noreply.github.com>
2022-08-18 14:07:41 +0200

    API - Figurine

---
+++ sources/APIpublic/listeFigurine.php
@@ -0,0 +1,49 @@
+<?php
+include 'APIHeader.php';
+$idFaction = filter($_GET['idFaction']);
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+  // Requête
+  // Trie figurine
+  $select = "SELECT `idFigurine`, `faction`, `role`, `taille`, `DC`, `DQM`, `mouvement`, `svg`, `pdv`, `vol`, `volStation`, `nomFigurine`, `texteFigurine`
+              FROM `figurines`
+              WHERE `faction` = :idFaction AND `figurineValide` = 1";
+  $param = [['prep'=>':idFaction', 'variable'=>$idFaction]];
+  $readFigurines = new RCUD($select, $param);
+  $dataFigurines = $readFigurines->READ();
+  $toutesFigurineUnFaction = array();
+  for ($i=0; $i <count($dataFigurines) ; $i++) {
+    $idF = [['prep'=>':id', 'variable'=>$dataFigurines[$i]['idFigurine']]];
+    //Recherche RS
+    $selectRS = "SELECT `nomRS`
+    FROM `RS_Figurine`
+    INNER JOIN `reglesSpeciales` ON `id_RS` = `idRS`
+    WHERE `id_Figurine` = :id";
+    $readRS = new RCUD($selectRS, $idF);
+    $dataRS = $readRS->READ();
+    array_push($dataFigurines[$i],$dataRS);
+    //Recherche Armes
+    $selectArmes = "SELECT  `idArme`, `nomArme`, `description`, `range`, `puissance`, `surPuissance`, `typeArme`, `couverture`, `cadenceTir`, `gabarit`
+    FROM `Armes_Figurine`
+    INNER JOIN `armes` ON `idArme` = `id_Arme`
+    WHERE `id_Figurine` = :id";
+    $readArmes = new RCUD($selectArmes, $idF);
+    $dataArmes = $readArmes->READ();
+    for ($j=0; $j <count($dataArmes) ; $j++) {
+      $selectRSArme = "SELECT `nomRS`
+      FROM `ArmesRS`
+      INNER JOIN `reglesSpeciales` ON `id_RS` = `idRS`
+      WHERE `id_Arme` = :idArme";
+      $paramIdArme = [['prep'=>':idArme', 'variable'=>$dataArmes[$j]['idArme']]];
+      $readRSArme = new RCUD($selectRSArme, $paramIdArme);
+      $readRSA = $readRSArme->READ();
+        array_push($dataArmes[$j],$readRSA);
+    }
+    array_push($dataFigurines[$i],$dataArmes);
+  }
+  //print_r($dataFigurines);
+  //echo '<pre>';
+  echo JSON_encode($dataFigurines);
+  //echo '</pre>';
+} else {
+  echo json_encode(['error' => 'Aucune donnée']);
+}
--- sources/APIpublic/listeUnivers.php
+++ sources/APIpublic/listeUnivers.php
@@ -1,9 +1,13 @@
 <?php
 include 'APIHeader.php';
-// Requête
-$select = "SELECT `idUnivers`, `nomUnivers`, `NT` FROM `univers` WHERE `valide` = 1";
-            $param = array();
-$readData = new RCUD($select, $param);
-$dataUnivers = $readData->READ();
-$data_Univers_JSON = JSON_encode($dataUnivers);
-echo $data_Univers_JSON;
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+  // Requête
+  $select = "SELECT `idUnivers`, `nomUnivers`, `NT` FROM `univers` WHERE `valide` = 1";
+              $param = array();
+  $readData = new RCUD($select, $param);
+  $dataUnivers = $readData->READ();
+  $data_Univers_JSON = JSON_encode($dataUnivers);
+  echo $data_Univers_JSON;
+} else {
+  echo json_encode(['error' => 'Aucune donnée']);
+}
